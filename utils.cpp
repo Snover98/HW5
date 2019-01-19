ccd8965417351fc3b4f80372e37c140c8bb09d65
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-01-19 15:50:18 +0200

    made data of strings conform to MIPS syntax

--- utils.cpp
+++ utils.cpp
@@ -67,7 +67,7 @@ std::string genStrData(std::string str){
 
     std::string str_label = "str_" + numToString(++num_strings);
     CodeBuffer::instance().emitData(str_label + ":");
-    CodeBuffer::instance().emitData(str);
+    CodeBuffer::instance().emitData(".ascii "+str);
 
     string_labels[str] = str_label;
 
