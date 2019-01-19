c4405d38b36642f41883228a7a1a439342999e70
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-01-19 13:03:58 +0200

    added functions for saving and loading structs

--- func_generation.cpp
+++ func_generation.cpp
@@ -5,12 +5,12 @@
 #include "func_generation.h"
 
 
-int emitFuncStart(std::string& func_name){
+int emitFuncStart(std::string &func_name) {
     emitComment("CODE OF FUNCTION " + func_name);
     return emit(func_name + ":");
 }
 
-int emitSetupFuncCall(std::string& func_name, SymTable& table){
+int emitSetupFuncCall(std::string &func_name, SymTable &table) {
     //save all registers in stack
     emitComment("SAVING REGISTERS");
     emitSaveRegisters();
@@ -23,56 +23,66 @@ int emitSetupFuncCall(std::string& func_name, SymTable& table){
     //after this the arguments should be put in the stack
 }
 
-int updateSPBeforeCall(std::string& func_name, SymTable& table){
+int updateSPBeforeCall(std::string &func_name, SymTable &table) {
     emitComment("update $sp according to the symbol table");
-    return emit("add $sp, $fp, (-" + numToString(table.nextOffset()) +")");
+    return emit("add $sp, $fp, (-" + numToString(table.nextOffset()) + ")");
 }
 
-int emitSaveRegisters(){
+int emitSaveRegisters() {
     int last_command = -1;
-    for(int i=0; i<NUM_OF_REGISTERS; ++i){
+    for (int i = 0; i < NUM_OF_REGISTERS; ++i) {
         last_command = emitPushReg(i);
     }
 
     return last_command;
 }
 
-int emitPushReg(int reg_num){
+int emitPushReg(int reg_num) {
     addPlaceInStack();
-    return emit("sw " + regName(reg_num) +", ($sp)");
+    return emit("sw " + regName(reg_num) + ", ($sp)");
 }
 
-int emitLoadRegisters(){
+int emitLoadRegisters() {
     int last_command = -1;
-    for(int i=NUM_OF_REGISTERS-1; i>=0; --i){
+    for (int i = NUM_OF_REGISTERS - 1; i >= 0; --i) {
         last_command = emitPopReg(i);
     }
 
     return last_command;
 }
 
-int emitPopReg(int reg_num){
-    emit("lw " + regName(reg_num) +", ($sp)");
+int emitPopReg(int reg_num) {
+    emit("lw " + regName(reg_num) + ", ($sp)");
     return removePlaceInStack();
 }
 
-int emitLoadVar(int reg_num, int offset){
+int emitLoadVar(int reg_num, int offset) {
     offset *= 4;
     return emit("lw " + regName(reg_num) + ", " + numToString(-offset) + "($fp)");
 }
 
-int emitSaveVar(int reg_num, int offset){
+int emitSaveVar(int reg_num, int offset) {
     offset *= 4;
     return emit("sw " + regName(reg_num) + ", " + numToString(-offset) + "($fp)");
 }
 
-int emitLoadVar(int reg_num, std::string& ID, SymTable& table){
+int emitLoadVar(int reg_num, std::string &ID, SymTable &table) {
     emitComment("load the variable " + ID + "into the register " + regName(reg_num));
     return emitLoadVar(reg_num, table.getSymbolEntry(ID).offset);
 }
 
-int emitSaveVar(int reg_num, std::string& ID, SymTable& table){
+int emitSaveVar(int reg_num, std::string &ID, SymTable &table) {
     emitComment("save the variable " + ID + "from the register " + regName(reg_num) + "into the stack");
     return emitSaveVar(reg_num, table.getSymbolEntry(ID).offset);
 }
 
+int emitLoadStructField(int reg_num, std::string &struct_name, std::string &field_name, SymTable &table, StructType &t) {
+    emitComment("Load the value of the field " + struct_name + "." + field_name + "into " + regName(reg_num));
+    return emitLoadVar(reg_num, table.getSymbolEntry(struct_name).offset + t.fieldOffset(field_name));
+}
+
+
+int emitSaveStructField(int reg_num, std::string &struct_name, std::string &field_name, SymTable &table, StructType &t) {
+    emitComment("Save the value of the field " + struct_name + "." + field_name + "from " + regName(reg_num));
+    return emitSaveVar(reg_num, table.getSymbolEntry(struct_name).offset + t.fieldOffset(field_name));
+}
--- func_generation.h
+++ func_generation.h
@@ -11,26 +11,41 @@
 #include "register_handler.h"
 
 // emit label for function code + comments
-int emitFuncStart(std::string& func_name);
+int emitFuncStart(std::string &func_name);
+
 // setup function call BEFORE pushing arguments into the stack in order
-int emitSetupFuncCall(std::string& func_name, SymTable& table);
+int emitSetupFuncCall(std::string &func_name, SymTable &table);
+
 // make sure $sp is the correct value before starting to push function arguments
-int updateSPBeforeCall(std::string& func_name, SymTable& table);
+int updateSPBeforeCall(std::string &func_name, SymTable &table);
+
 // save all registers in stack
 int emitSaveRegisters();
+
 // function for pushing value in the register into the stack
 int emitPushReg(int reg_num);
+
 //function for popping registers from stack
 int emitLoadRegisters();
+
 //function for pooping a value into a register
 int emitPopReg(int reg_num);
+
 //load variable from stack into register, offset is in number of bytes
 int emitLoadVar(int reg_num, int offset);
+
 //save variable from register into stack, offset is in number of bytes
 int emitSaveVar(int reg_num, int offset);
+
 //load & save var with ID and symbol table
-int emitLoadVar(int reg_num, std::string& ID, SymTable& table);
-int emitSaveVar(int reg_num, std::string& ID, SymTable& table);
+int emitLoadVar(int reg_num, std::string &ID, SymTable &table);
+
+int emitSaveVar(int reg_num, std::string &ID, SymTable &table);
+
+//functions for struct fields
+int emitLoadStructField(int reg_num, std::string &struct_name, std::string &field_name, SymTable &table, StructType &t);
+
+int emitSaveStructField(int reg_num, std::string &struct_name, std::string &field_name, SymTable &table, StructType &t);
 
 
 #endif //HW5_FUNC_GENERATION_H
